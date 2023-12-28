9d81d7e9d74efb58e2bfb43e0deed6107e3e874c
TheAimHero <vsghodekar1@gmail.com>
2023-12-28 16:36:33 +0530

    feat: Parse let and return statement without expression

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,9 +4,9 @@ import "github.com/TheAimHero/goInterpreter/token"
 
 type Lexer struct {
 	input        string
-	position     int  // current position in input (points to current char)
-	readPosition int  // current reading position in input (after current char)
-	ch           byte // current char under examination
+	position     int
+	readPosition int
+	ch           byte
 }
 
 func New(input string) *Lexer {

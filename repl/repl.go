b5077fe8ae4cfb6646266987a6228aacc21cecd0
TheAimHero <vsghodekar1@gmail.com>
2023-12-30 15:10:59 +0530

    feat: added evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/TheAimHero/goInterpreter/evaluator"
 	"github.com/TheAimHero/goInterpreter/lexer"
+	"github.com/TheAimHero/goInterpreter/object"
 	"github.com/TheAimHero/goInterpreter/parser"
 )
 
@@ -13,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -31,8 +34,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
